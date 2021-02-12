3c202ebad8e290251baf76f0ef61235e95352e75
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-12 15:18:26 -0800

    add kegcontrol and fix small bugs, currently running

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,27 +1,52 @@
 import React from 'react';
 import KegList from './KegList';
+import NewKegForm from './NewKegForm';
 
 
 class KegControl extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      masterKegList = []
+      formVisibleOnPage: false,
+      masterKegList: []
     };
+  }
+
+  handleClick = () => {
+    if(this.state.selectedKeg != null) {
+      this.setState({
+
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage
+      }));
+    }
+  }
+
+  handleAddingNewKegToList = (newKeg) => {
+    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    this.setState({
+      masterKegList: newMasterKegList,
+      formVisibleOnPage: false
+    });
   }
 
   render () {
     let currentlyVisibleState = null;
     let buttonText = null;
-    // if(){
-        //ADD form visible on page
-    // } else {
-    //   currentlyVisibleState = <KegList kegList = {this.state.masterKegList} />;
-    //   buttonText = "Add New Keg";
-    // }
+    if(this.state.formVisibleOnPage){
+      currentlyVisibleState = <NewKegForm onNewKegCreation = {this.handleAddingNewKegToList} />
+      buttonText = "Return to List";
+    } else {
+      currentlyVisibleState = <KegList kegList = {this.state.masterKegList} />;
+      buttonText = 'Add New Keg';
+    }
 
     return (
       <>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </>
     );
   }
