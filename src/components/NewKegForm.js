94ee86a8bb7c8af35535d1d527cee10f8211068e
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-12 14:26:01 -0800

    add newkegform component

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,36 @@
+
+import React from 'react';
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+import ReusableForm from './ReusableForm';
+
+function NewKegForm() {
+
+  function handleNewKegFormSubmission(event){
+    event.preventDefault();
+    PropTypes.onNewKegCreation({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      alcoholContent: event.target.brand.value,
+      price: event.target.price.value,
+      flavor: event.target.flavor.value,
+      description: event.target.description.value,
+      quantity: event.target.quantity.value,
+      id: v4()
+    });
+  }
+
+  return(
+    <>
+      <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Submit" />
+    </>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
