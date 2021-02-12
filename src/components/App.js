e4572ec5f6c966e0c08fb205377031059a6766af
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-12 13:40:27 -0800

    add component files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <>
+      <Header /> 
+      <div className="container">
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header(){
+  return(
+    <h1 className="display-2 d-block p-2 bg-secondary text-white">Tap-Room</h1>
+  );
+}
+
+export default Header;
