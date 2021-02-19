40ffd5b469945e88818614f98f540c4584f8eeb6
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-19 18:36:36 -0500

    pass test 3 for selected-keg-reducer

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -4,6 +4,8 @@ export default (state = null, action) => {
   switch(action.type){
     case c.SELECT_KEG:
         return action.keg;
+    case c.DESELECT_KEG:
+      return null;
     default:
       return state; 
   }
