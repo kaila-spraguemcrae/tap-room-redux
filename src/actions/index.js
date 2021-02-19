eae7946b5416698b8bbb90172a9791349f9c59c5
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-19 13:55:54 -0500

    pass addKeg action test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
 import * as c from './ActionTypes';
 
 export const addKeg = (keg) => {
-  
+  const {name, brand, alcoholContent, price, flavor, description, quantity, id} = keg;
   return {
- 
+    type: c.ADD_KEG,
+    name,
+    brand,
+    alcoholContent,
+    price,
+    flavor,
+    description,
+    quantity,
+    id
   }
 }
