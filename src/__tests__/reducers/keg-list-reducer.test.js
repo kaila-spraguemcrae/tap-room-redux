81394e2a12d112b8bed526cf965f99d9847bb031
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-19 13:19:52 -0500

    good fail for keg-list-reducer test 2

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -2,7 +2,47 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
 
+  let action;
+
+  const kegData = {
+    name: "test",
+    brand: "test",
+    alcoholContent: "test",
+    price: "test",
+    flavor: "test",
+    description: "test",
+    quantity: 24,
+    id: 1
+  }
+
   test('Should return default dtate if there is not action type passed to the reducer', ()=> {
     expect(kegListReducer({}, {type:null})).toEqual({});
+  });
+
+  test('Should add a keg to the masterKegList', ()=> {
+    const { name, brand, alcoholContent, price, flavor, decription, quantity, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name,
+      brand,
+      alcoholContent,
+      price,
+      flavor,
+      decription,
+      quantity,
+      id
+    }
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name,
+        brand,
+        alcoholContent,
+        price,
+        flavor,
+        decription,
+        quantity,
+        id
+      }
+    });
   });
 });
