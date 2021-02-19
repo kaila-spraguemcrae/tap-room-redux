49cfb1b7490a55affef7e0552d9ff7c7455b6ffa
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-19 13:26:20 -0500

    pass keg-list-reducer test 2 and add redux to project

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,4 +1,20 @@
 export default (state = {}, action) => {
-  
-    return state;
+  const {name, brand, alcoholContent, price, flavor, decription, quantity, id} = action;
+  switch(action.type){
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id]: {
+          name,
+          brand,
+          alcoholContent,
+          price,
+          flavor,
+          decription,
+          quantity,
+          id
+        }
+      });
+    default:
+      return state;
+  }
 }
