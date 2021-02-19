059323d119190fa9aa68077c2c04431030c66b3b
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-19 18:14:05 -0500

    good fail for selected-keg-reducer test 2

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -4,7 +4,22 @@ import * as a from '../../actions/index';
 describe('selectedKegReducer', () => {
   let action;
 
+  const keg = {
+    name: "test",
+    brand: "test",
+    alcoholContent: "test",
+    price: "test",
+    flavor: "test",
+    description: "test",
+    quantity: 24,
+    id: 1
+  }
+
   test('Should return default state if no type is recognized', () => {
     expect(selectedKegReducer({}, {type: null})).toEqual({});
+  });
+
+  test('Should return selected keg when SELECTED_KEG is called', () => {
+    expect(selectedKegReducer({}, action)).toEqual(keg);
   });
 });
