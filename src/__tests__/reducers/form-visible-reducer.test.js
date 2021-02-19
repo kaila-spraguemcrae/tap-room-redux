090266f5ca91ec91ff53b25f8150dfe1a5d25c55
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-19 15:59:04 -0500

    good fail for form-visible-reducer test 2

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -5,4 +5,8 @@ describe ('formVisibleReducer', ()=> {
   test('Should return default state if no action type is recognized', ()=> {
     expect(formVisibleReducer(false, {type: null})).toEqual(false);
   });
+
+  test('Should return true if toggle action is passed', () => {
+    expect(formVisibleReducer(false,{type: 'TOGGLE_FORM'})).toEqual(true);
+  });
 });
