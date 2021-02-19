3ad36ab139f52cdccd9a4106dfb8eb96886a90d6
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-19 16:01:00 -0500

    pass for form-visible-reducer test 2

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -1,5 +1,10 @@
 import * as c from './../actions/ActionTypes';
 
 export default (state = false, action) => {
-  return state;
+  switch (action.type) {
+    case 'TOGGLE_FORM':
+      return !state;
+  default:
+    return state;
+  }
 };
